46d69d849bd6e26a90e5dd0a8969c908521154ea
MinChi-0314 <57542261+MinChi-0314@users.noreply.github.com>
2020-09-14 12:47:40 +0800

    Update 教室規格.R

--- 教室規格.R
+++ 教室規格.R
@@ -1,13 +1,8 @@
-library(dplyr)
-library(stringi)
-
-########## Parameters ##########
-### Size of class
+### Size of class in NTU, College of Social Sciences 
 class101 <- c(5,9,5,17)
+class301 <- c(8,7)
+class302 <- c(8,7)
+class305 <- c(4,4,7)
+class306 <- c(4,4,7)
 class403 <- c(2,6,2,14)
 class502 <- c(2,6,2,14)
-
-getwd() %>% 
-  sub("Documents",replacement = "Google ¶³ºÝµwºÐ/NTU/Campus",.) %>%
-  paste0(.,"/classSize.Rdata") %>%
-  save(class101, class403, class502, file= .)
