5c638bc6082dbcb7e5db601daa296ca2f4e02cc7
MinChi-0314 <57542261+MinChi-0314@users.noreply.github.com>
2020-09-14 13:17:24 +0800

    Delete 教室規格.R

--- 教室規格.R
+++
@@ -1,8 +0,0 @@
-### Size of class in NTU, College of Social Sciences 
-class101 <- c(5,9,5,17)
-class301 <- c(8,7)
-class302 <- c(8,7)
-class305 <- c(4,4,7)
-class306 <- c(4,4,7)
-class403 <- c(2,6,2,14)
-class502 <- c(2,6,2,14)
