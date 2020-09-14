46530fee15b3e59fd811098fcb46ee58383c074d
MinChi-0314 <57542261+MinChi-0314@users.noreply.github.com>
2020-09-14 22:41:54 +0800

    Update arrange seat.R

--- arrange seat.R
+++ arrange seat.R
@@ -1,9 +1,17 @@
-library(httr)
 library(tidyverse)
+library(httr)
+library(readxl)
 
+## import class size
 class <- GET("https://raw.githubusercontent.com/MinChi-0314/seat/master/NTU.csv") %>%
   content("text") %>% 
   str_split(pattern = "\r\n") %>% 
   unlist %>% .[-c(1,9)]
 
-#class %>% grep(pattern = "SS301",value=T) %>% str_extract(regex("(?<=,).*"))
+## deal with student list 
+data <- read_xlsx("C:/Users/Username/Desktop/1091_統計學暨實習_修課學生名單_選課結果202009082144.xlsx")[-c(1:3),c(3,6)]
+colnames(data) <- c("department", "name")
+
+
+
+
